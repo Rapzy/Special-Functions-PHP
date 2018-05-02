16fe7bada459a25d703af3629c4c9812647f8dc6
Egor <andrushko.egor@gmail.com>
2018-05-02 19:34:41 +0300

    Add English and Refactor code
    Working with English language and negative numbers

--- script.php
+++ script.php
@@ -1,50 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Special Functions PHP</title>
+</head>
+<body>
+	<style type="text/css">
+		body {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			min-height: 100vh;
+			margin: 0;
+		}
+		div {
+			text-align: center;
+		}
+		#number{
+			font-size: 100px;
+		}
+		#text,a{
+			font-size: 30px;
+		}
+	</style>
+
+</body>
+</html>
 <?php
 	$str_in = htmlentities($_GET['str_in']);
 	$lang = $_GET['lang'];
-	$digits = [
-		"ru" => [
-			["","один","два","три","четыре","пять","шесть","семь","восемь","девять","десять","одиннадцать","двенадцать","тринадцать","четырнадцать","пятнадцать","шестнадцать","семнадцать","восемнадцать","девятнадцать"],
-			["","десять","двадцать","тридцать","сорок","пятьдесят","семьдесят","восемдесят","девяноста"],
-			["","сто","двести","триста","четыреста","пятьсот","шестьсот","семьсот","восемьсот","девятьсот"]
-		],
-		"en" => ["","one","two","three","four","five","six","seven","eight","nine"]
-	];
-	$words = [
-		["","тысяча","миллион","миллиард","триллион","квадриллион","квинтиллион"],
-		["","тысячи","миллиона","миллиарда","триллиона","квадриллиона","квинтиллиона"],
-		["","тысяч","миллионов","миллиардов","триллионов","квадриллионов","квинтиллионов"]
-	];
-	$len = strlen($str_in);
-	$pos = $len;
-	$word="";
-	for ($i=0; $i < $len; $i++) {
-		$digit = 1 * substr($str_in, $i, 1);
-		if ($digit==1 && ($len-1-$i)%3==1) {
-			$i++;
-			$digit = 1 * substr($str_in, $i, 1);
-			$digit+=10;
+	$str_in = str_replace(array(',', ' ','.'), '' , trim($str_in));
+	$str_out = ExpressNumToWord($str_in,$lang);
+	for ($i = strlen($str_in) - 1; $i >= 0; $i--) { 
+		if ((strlen($str_in) - $i) % 3 == 0) {
+			$str_in = substr_replace($str_in, " ", $i,0);	
 		}
-		if ($pos>0 && ($len-1-$i)%3==0){
-			if ($digit > 1 && $digit < 5) {
-				$case = 1;
-			}
-			elseif ($digit >= 5) {
-				$case = 2;
+	}
+	echo "<div><span id='number'>$str_in</span><br><span id='text'>$str_out</span>";
+	if ($lang == 'en') {
+		echo "<br><a href='index.html'>Home</a></div>";
+	}
+	else{
+		echo "<br><a href='index.html'>На главную</a></div>";
+	}
+	function ExpressNumToWord($str_in, $lang)
+	{
+		$digits = [
+			"ru" => [
+				["ноль","один","два","три","четыре","пять","шесть","семь","восемь","девять","десять","одиннадцать","двенадцать","тринадцать","четырнадцать","пятнадцать","шестнадцать","семнадцать","восемнадцать","девятнадцать"],
+				["","десять","двадцать","тридцать","сорок","пятьдесят","шестьдесят","семьдесят","восемдесят","девяноста"],
+				["","сто","двести","триста","четыреста","пятьсот","шестьсот","семьсот","восемьсот","девятьсот"]
+			],
+			"en" => [
+				["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"],
+				["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+			] 
+		];
+		$words = [
+			"ru" => [
+				["","тысяча","миллион","миллиард","триллион","квадриллион","квинтиллион"],
+				["","тысячи","миллиона","миллиарда","триллиона","квадриллиона","квинтиллиона"],
+				["","тысяч","миллионов","миллиардов","триллионов","квадриллионов","квинтиллионов"]
+			],
+			"en" => [
+				["","thousand","million","billion","trillion","quadrillion","quintillion"]
+			]
+		];
+		$word = "";
+		$str_out = "";
+		if (substr($str_in, 0, 1) == '-'){
+			if ($lang == 'ru') {
+				$str_out .= "минус ";
 			}
 			else{
-				$case = 0;
+				$str_out .= "minus ";
 			}
-			$word = $words[$case][intdiv($pos-1,3)];
-			$pos = $len-$i-1;
+			$str_in = substr($str_in, 1);
 		}
-		echo $digits[$lang][($len-1-$i)%3][$digit]." ".$word." ";
-		$word="";
-	}
-	for ($i=$len-1; $i >= 0; $i--) { 
-		if (($len-$i)%3==0) {
-			$str_in=substr_replace($str_in, " ", $i,0);	
+		$len = strlen($str_in);
+		$pos = $len; 
+		for ($i = 1; $i <= $len; $i++) {
+			$digit = 1 * substr($str_in, $i-1, 1);
+			$place = $len - $i;
+			if ($digit == 1 && $place % 3 == 1) {
+				$i++;
+				$digit = 1 * substr($str_in, $i-1, 1);
+				$digit += 10;
+				$place = $len - $i;
+			}
+			if ($pos > 0 && $place % 3 == 0){
+				if ($digit == 1 || $lang == "en") {
+					$case = 0;
+				}
+				elseif ($digit > 1 && $digit < 5) {
+					$case = 1;
+				}
+				elseif ($digit >= 5) {
+					$case = 2;
+				}
+				if (intdiv($pos - 1, 3)) {
+					$word = $words[$lang][$case][intdiv($pos - 1, 3)];
+				}
+				$pos = $place;
+			}
+			if ($lang == 'en') {
+				if ($place % 3 == 2) {
+					$word = "hundred";
+				}
+				if($place % 3 == 1){
+					$str_out .= $digits[$lang][1][$digit]." ".$word." ";
+				}
+				else
+				{
+					$str_out .= $digits[$lang][0][$digit]." ".$word." ";
+				}
+			}
+			else {
+				$str_out .= $digits[$lang][$place % 3][$digit]." ".$word." ";
+			}
+			$word = "";
 		}
+		return $str_out;
 	}
-	echo "<br>$str_in";
-
-	echo "<br><a href='index.html'>Home</a>";
